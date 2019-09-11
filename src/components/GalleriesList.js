f0526c19e026fb40a7de2a9a027474c7c8e0f830
Michael Gellings <michael.gellings@gmx.net>
2019-09-11 12:10:26 +0200

    create file structure with placeholder components

---
+++ src/components/GalleriesList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Gallery from "./Gallery";
+
+export default function GalleriesList() {
+  return (
+    <div>
+      GalleriesList
+      <Gallery />
+      <Gallery />
+    </div>
+  );
+}
